36799da8be1bf989aba7f7b3110ac24a5a884e2e
Corey <angelusproductions@gmail.com>
2019-04-30 18:25:54 -0400

    take care of all minor issues

--- ReactReduxBoilerPlate/ClientApp/src/store/configureStore.js
+++ ReactReduxBoilerPlate/ClientApp/src/store/configureStore.js
@@ -1,13 +1,10 @@
 ﻿import { applyMiddleware, combineReducers, compose, createStore } from 'redux';
 import thunk from 'redux-thunk';
 import { routerReducer, routerMiddleware } from 'react-router-redux';
-import * as Counter from './Counter';
-import * as WeatherForecasts from './WeatherForecasts';
+
 
 export default function configureStore(history, initialState) {
   const reducers = {
-    counter: Counter.reducer,
-    weatherForecasts: WeatherForecasts.reducer
   };
 
   const middleware = [
@@ -17,10 +14,10 @@ export default function configureStore(history, initialState) {
 
   // In development, use the browser's Redux dev tools extension if installed
   const enhancers = [];
-  const isDevelopment = process.env.NODE_ENV === 'development';
-  if (isDevelopment && typeof window !== 'undefined' && window.devToolsExtension) {
-    enhancers.push(window.devToolsExtension());
-  }
+  //const isDevelopment = process.env.NODE_ENV === 'development';
+  //if (isDevelopment && typeof window !== 'undefined' && window.devToolsExtension) {
+  //  enhancers.push(window.devToolsExtension());
+  //}
 
   const rootReducer = combineReducers({
     ...reducers,
